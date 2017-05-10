f77eec11b604893185dd1ffc022ae3e8240273dd
John Dowd and Roger Tobias <john+roger@>
2017-05-10 10:21:19 -0700

    adds the functionality for obtaining the players names.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,29 @@
-function getRoll {
+//--------------------FRONTEND LOGIC
+$(document).ready(function() {
+  $("#players").submit(function(event) {
+    event.preventDefault();
+    debugger;
+    var playerOne = $("input#inputtedPlayerOne").val();
+    var playerTwo = $("input#inputtedPlayerTwo").val();
+
+    var playerOne = new PlayerOne(playerOne);
+    var playerTwo = new PlayerTwo(playerTwo);
+
+    $("#intro").hide();
+    $("#main").show();
+  });
+});
+//--------------------BACKEND LOGIC
+function GetRoll() {
   return Math.floor(Math.random() * 6) + 1;
+};
+
+function PlayerOne(playerOne = "", score) {
+  this.name = playerOne;
+  this.score = [];
+};
+
+function PlayerTwo(playerTwo = "", score) {
+  this.name = playerTwo;
+  this.score = [];
 };
